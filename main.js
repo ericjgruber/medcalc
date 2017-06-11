16888c895813b4af9531cdd6e9af4055cf5d79f1
Eric J Gruber <Eric.Gruber@vml.com>
2017-06-11 16:22:47 -0500

    Finish the html table and use the client-provided formulas

--- main.js
+++ main.js
@@ -17,11 +17,12 @@ var vm = new Vue({
     calculatedTotals: function() {
       // BMI formula taken from
       // http://extoxnet.orst.edu/faqs/dietcancer/web2/twohowto.html
+      // BMI (Body mass index): (w/2.2)/(0.0254H) 2 = X kg/m 2
 
       var weight = jQuery('input[name="weight"]').val();
-      var weightMetric = weight * 0.45;
+      var weightMetric = weight / 2.2;
       var heightInches = jQuery('input[name="height"]').val();
-      var heightMetric = heightInches * 0.025;
+      var heightMetric = heightInches * 0.0254;
       var heightResult = heightMetric * heightMetric;
       var age = jQuery('input[name="age"]').val();
       var height = jQuery('input[name="height"]').val();
@@ -32,15 +33,19 @@ var vm = new Vue({
       var bmiTotals = bmiResult.toFixed();
 
       // Get BMR:
-      // Men: 66 + (6.2 * weight in lbs.) + (12.7 * height in inches) - (6.76 * age in years)
-      // Women: 655.1 + ( 4.35 × weight in pounds ) + ( 4.7 × height in inches ) - ( 4.7 × age in years )
       // Male
       if (gender == "male") {
-        var bmrTotals = 66 + (6.2 * weight) + (12.7 * heightInches) - (6.76 * age);
+        // (old, but correct?)
+        // var bmrTotals = 66 + (6.2 * weight) + (12.7 * heightInches) - (6.76 * age);
+        // Client provided: (10w/2.2) + (0.15875h) – (5A) + 5
+        var bmrTotals = (10 * weight/2.2) + (0.15875 * heightInches) - (5 * age) + 5;
       }
       // Female
       else if (gender == "female") {
-        var bmrTotals = 655.1 + (4.35 * weight) + (4.7 * heightInches) - (4.7 * age);
+        // (old, but correct?
+        // var bmrTotals = 655.1 + (4.35 * weight) + (4.7 * heightInches) - (4.7 * age);
+        // Client provided: (10w/2.2) + (0.15875h) – (5A) – 161
+        var bmrTotals = (10 * weight/2.2) + (0.15875 * heightInches) - (5 * age) - 161;
       }
 
       var bmrTotals = bmrTotals.toFixed();
@@ -68,8 +73,8 @@ var vm = new Vue({
 
       var activity = activity.toFixed();
 
-      jQuery('#bmrResult').html("<strong>Your BMR is " + bmrTotals + " calories.</strong>");
-      jQuery('#bmiResult').html("<strong>Your BMI is " + bmiTotals + "</strong>");
+      jQuery('#bmiResult').html("<strong>Body Mass Index (BMI) is " + bmiTotals + "</strong>");
+      jQuery('#bmrResult').html("<strong>Basal Metabolic Rate (BMR) is " + bmrTotals + " calories.</strong>");
       jQuery('#suggestedActivity').html("<strong>Your suggested caloric intake based on your activity level is " + activity + " calories</strong>");
 
     }
